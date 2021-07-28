0218552b467df15533fb51e6bc7d46563ce46cf0
NatanMacielC <natan.macielc@gmail.com>
2021-07-28 19:07:38 -0300

    first commit

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Cnpj { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string RazaoSocial { get; set; }
+        public DateTime DataCadastro { get; set; }
+     
+    }
+}
